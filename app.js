e92602ea83e364bf19e1ac2dd2d3cfa9c6756419
King QuytU <quib2012247@student.ctu.edu.vn>
2023-09-20 08:08:36 +0700

    update

--- app.js
+++ app.js
@@ -13,15 +13,15 @@ app.get("/", (req, res) => {
     message: "Welcome to contact book application.",
   });
 });
-
-// define error-handling middleware last, after other app.use() and routes calls
-app.use((err, req, res, next) => {
-  return res.status(error.statusCode || 500).json({
-    message: error.message || "Internal Server Error",
-  });
-});
 // handle 404 response
 app.use((req, res, next) => {
   return next(new ApiError(404, " not found"));
 });
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
 module.exports = app;
